0634c21bac5988decdef891182cd429eab182ed5
eferrer686 <edfmgr@gmail.com>
2018-04-16 14:21:36 -0500

    Simulink Final

--- procesar.m
+++ procesar.m
@@ -3,17 +3,17 @@ function [y] = procesar(u)
 %   Detailed explanation goes here
 
 M = u;
-
+%del = [1 2 3 4 5 6 7 8 9];
 
 M(any(M(:,3)==-1,2),:)=[];
 M(any(M(:,3)==0,2),:)=[];
 
 
-as=size(del,2);
-for c=1:as
+%as=size(del,2);
+%for c=1:as
    
-    M(:,1)=[];
-end
+ %   M(:,1)=[];
+%end
 
 y = M;
 
